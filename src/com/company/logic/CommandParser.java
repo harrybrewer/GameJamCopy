207d967a851cae4f10e5b437dc1c80fa45258462
Clark <gc16077@essex.ac.uk>
2019-01-26 11:27:36 +0000

    Adding additional edits to git

---
+++ src/com/company/logic/CommandParser.java
@@ -0,0 +1,15 @@
+package com.company.logic;
+
+public class CommandParser {
+
+    static String[] parseCommand(String plainText){
+        String tokens[] = plainText.split(" ");
+
+        for(String t : tokens){
+            System.out.println(t);
+        }
+        return tokens;
+    }
+
+
+}
